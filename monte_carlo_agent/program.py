24ccede42474b69ace4a57890f1c4ef4cddde420
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-04-24 02:24:58 +1000

    refactoring monte carlo

--- monte_carlo_agent/program.py
+++ monte_carlo_agent/program.py
@@ -10,6 +10,7 @@ import random
 TOTAL_RUNS = 100
 MAX_TURNS = 150
 C = 2
+EXPANSION_FACTOR = 10
 
 class Agent:
     """
@@ -29,6 +30,7 @@ class Agent:
 
     def action(self, **referee: dict) -> Action:
         """
+        (STRATEGY) Do straight monte carlo tree search
         Do a monte carlo tree search given a node
         """
         # RED = 0
@@ -58,121 +60,96 @@ class Agent:
         # all moves after the first one 
         # (MONTE CARLO)
 
-        runs = 0
-        while (runs < TOTAL_RUNS):
+        for _ in range(TOTAL_RUNS):
 
-            node = self.monte_carlo_root
+            root = self.monte_carlo_root
 
             # selection
-            while node.children:
+            leaf = select(root)
 
-                # select the highest UBC1 node
-                highest_ubc1_node = None
-                highest_ubc1 = -1
-                for child in node.children:
-                    if child.ubc1 > highest_ubc1:
-                        highest_ubc1 = child.ubc1
-                        highest_ubc1_node = child
-                    # TODO can optimise by short circuiting
+            # expansion
+            child = expand(leaf)
 
-                # make the highest UBC1 node the next node in the chain
-                node = highest_ubc1_node
-            
-            # now should be at a leaf node
-            if node.n == 0:
-                # hasn't been rolled out yet
-                res = rollout(node.state, node.turn, node.player)
-                node.increase_t(res)
-                node.calculate_ubc1(runs)
-            else:
-                # expand one state
-                possible_moves = get_possible_moves(node.state, node.player)
-                random_next_move = possible_moves[random.randrange(0, len(possible_moves))]
-                next_state = get_next_state(node.state, random_next_move, node.player)
-                next_player = None
-                if node.player == PlayerColor.RED:
-                    next_player = PlayerColor.BLUE
-                else:
-                    next_player = PlayerColor.RED
-                new_node = MonteCarloNode(next_state, node, node.turn + 1, next_player)
+            # simulation
+            result = rollout(child.state, child.turn, child.player)
 
-                # rollout
-                res = rollout(new_node.state, new_node.turn, new_node.player)
-                new_node.increase_t(res)
-                node.calculate_ubc1(runs)
-            
             # backpropogate
+            backpropogate(result, child)
 
-            runs += 1
 
-
-        possible_moves = get_possible_moves(self.current_state, self._color)
-        size = len(possible_moves)
-        assert size > 0
-        return possible_moves[random.randrange(0, size)]
+        # selecting highest UBC from immediate children
+        highest_ubc1 = -1
+        highest_ubc1_action = None
+        for child in self.monte_carlo_root.children:
+            ubc1 = get_ubc1(child)
+            if ubc1 > highest_ubc1:
+                highest_ubc1 = ubc1
+                highest_ubc1_action = self.monte_carlo_root.action
+        return highest_ubc1_action
 
     def update(self, color: PlayerColor, action: Action, **referee: dict):
         """
-        Updating the state
+        Updating the state after each turn
         """
 
-        if color == PlayerColor.RED:
-            next_player = PlayerColor.BLUE
-        else:
-            next_player = PlayerColor.RED
-        
+        next_player = get_next_player(color)
         self.monte_carlo_root = MonteCarloNode(get_next_state(self.monte_carlo_root.state, action, color), None, self.turn_count, next_player)
 
         self.turn_count += 1
             
 
-
 class MonteCarloNode:
-    def __init__(self, state: dict, parent, turn: int, player):
+    def __init__(self, state: dict, parent, turn: int, player: PlayerColor, action: PlaceAction):
         self.state = state
         self.t = 0
         self.n = 0
-        self.ubc1 = float('inf')
         self.children = []
         self.parent = parent
         self.turn = turn
         self.player = player
+        self.action = action
+
+        # we consider a node to be expanded if it has 10 children
+        # because i think it's easier to code
+        self.expanded = False
 
     def increase_t(self, score):
         self.t += score
         self.n += 1
     
-    def calculate_ubc1(self, runs):
-        assert self.n != 0
-        self.ubc1 = (self.t / self.n) + C * math.sqrt(math.log(runs / self.n))
-    
     def append_child(self, node):
+        # the function shouldn't be called if the node is already expanded
+        assert(len(self.children) < EXPANSION_FACTOR and self.expanded == False)
         self.children.append(node)
 
-def rollout(state: dict, current_turn: int, player: PlayerColor):
+        if len(self.children) == EXPANSION_FACTOR:
+            self.expanded = True
+
+
+def rollout(child: MonteCarloNode):
+    """
+    MONTE CARLO SIMULATION / ROLLOUT
+    """    
+
     # keep rolling out until one player has no available plays left or the max turns has been reached
-    current_player = player
-    current_state = state
+    current_player = child.player
+    current_state = child.state
+    
     while current_turn <= MAX_TURNS:
-        possible_moves = get_possible_moves(current_state, current_player)
-        if not possible_moves:
-            # this player has no possible moves, so the other player wins
-            if player == PlayerColor.BLUE:
+        random_move = get_random_move(current_state, current_player)
+
+        if not random_move:
+            # no possible moves
+
+            if current_player == PlayerColor.BLUE:
                 # RED WINS
                 return 0
             else:
                 # BLUE WINS
                 return 1
-            
-        # there are moves, so pick one at random and continue the rollout
-        next_action = possible_moves[random.randrange(0, len(possible_moves))]
-        current_state = get_next_state(current_state, next_action)
 
         current_turn += 1
-        if current_player == PlayerColor.RED:
-            current_player = PlayerColor.BLUE
-        else:
-            current_player = PlayerColor.RED
+        current_player = get_next_player(current_player)
 
     # max turns has been reached
     # return based on whoever has more squares
@@ -193,4 +170,63 @@ def rollout(state: dict, current_turn: int, player: PlayerColor):
     else:
         # blue won
         return 1
-            
+
+def get_random_move(state: dict, player: PlayerColor):
+    possible_moves = get_possible_moves(state, player)
+    if not possible_moves:
+        return None
+    random_move = possible_moves[random.randrange(0, len(possible_moves))]
+    return random_move
+
+def get_next_player(player: PlayerColor):
+    if player == PlayerColor.RED:
+        current_player = PlayerColor.BLUE
+    else:
+        current_player = PlayerColor.RED
+    return current_player
+
+def get_ubc1(node: MonteCarloNode):
+    assert node.parent != None # should never be calling UBC1 on the root node
+    return (node.t / node.n) + C * math.sqrt(math.log(node.parent.n) / node.n)
+
+
+def select(node: MonteCarloNode):
+    """
+    MONTE CARLO SELECTION
+    """    
+    current_node = node
+    while current_node.children:
+
+        # select the highest UBC1 node
+        highest_ubc1_node = current_node
+        highest_ubc1 = -1
+        for child in current_node.children:
+            ubc1 = get_ubc1(child)
+            if ubc1 > highest_ubc1:
+                highest_ubc1 = ubc1
+                highest_ubc1_node = child
+
+            # TODO can optimise by short circuiting
+            # TODO optimise by having ubc get called once and stored within the node
+
+        # make the highest UBC1 node the next node in the chain
+        current_node = highest_ubc1_node
+    
+    return current_node
+
+def expand(leaf: MonteCarloNode):
+    """
+    MONTE CARLO EXPANSION
+    """    
+    random_move = get_random_move(leaf.state, leaf.player)
+    next_state = get_next_state(leaf.state, random_move, leaf.player)
+    next_player = get_next_player(leaf.player)
+    return MonteCarloNode(next_state, leaf, leaf.turn + 1, next_player, random_move)
+
+def backpropogate(result, node: MonteCarloNode):
+    """
+    MONTE CARLO BACKPROPOGATION
+    """    
+    while node.parent:
+        node.increase_t(result)
+        node = node.parent
