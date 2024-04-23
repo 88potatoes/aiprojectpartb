1df1ff9b91ace5e8d67d95fa4e7397fdd0d23088
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-04-23 16:44:03 +1000

    initial rollout function + other monte carlo setup

---
+++ helpers.py
@@ -0,0 +1,164 @@
+from referee.game.actions import PlaceAction
+from referee.game.constants import BOARD_N
+from referee.game.coord import Coord, Direction
+from referee.game.player import PlayerColor
+
+
+def get_possible_moves_from_coord(board: dict, coord: Coord):
+    """
+    Given a state and a coordinate, returns all possible moves from that coordinate.
+    Returns a set of frozensets of Coords
+    """
+
+    path = [coord]
+    possible_moves = set() # set of frozensets
+
+    # generates all the possible moves
+    # accounts for traversing across borders
+    # returns an array of array of coords
+    def dfs(coord, piece_number):
+        if coord in path or coord in board:
+            return
+        
+        # add coord to path
+        path.append(coord)
+
+        # sprouting from piece 1
+        if piece_number == 1:
+
+            # find all open squares from the current square
+            open_coords = []
+            for direction in Direction:
+                new_coord = coord + direction
+                
+                # don't add square if it's already occupied
+                if new_coord in path or new_coord in board:
+                    continue
+                
+                open_coords.append(new_coord)
+
+            # add a branch and continue
+            for i in range(len(open_coords)):
+                path.append(open_coords[i])
+                for j in range(len(open_coords)):
+                    if i == j:
+                        continue
+                    assert len(path) == 3
+                    dfs(open_coords[j], piece_number + 2)
+                path.pop()
+                
+
+            # if 3 squares are open then add the T-piece
+            if len(open_coords) == 3:
+                assert len(path) == 2
+                possible_moves.add(frozenset([path[-1]] + open_coords))
+
+
+
+        # sprouting from piece 2 to find T-piece
+        if piece_number == 2:
+
+            # find all open squares from the current square
+            open_coords = []
+            for direction in Direction:
+                new_coord = coord + direction
+                
+                # don't add square if it's already occupied
+                if new_coord in path or new_coord in board:
+                    continue
+                
+                open_coords.append(new_coord)
+
+            # if there are more than two, then add combinations to find T-pieces
+            if len(open_coords) >= 2:
+                for i in range(len(open_coords)):
+                    for j in range(i+1, len(open_coords)):
+                        assert len(path) == 3
+                        possible_moves.add(frozenset(path[1:] + [open_coords[i], open_coords[j]]))
+
+
+        # placed the final block
+        if piece_number == 4:
+            assert len(path) == 5
+            possible_moves.add(frozenset(path[1:])) # take the last 4 to not include the original square
+            path.pop()
+            return
+        
+        # place the next block
+        for direction in Direction:
+            dfs(coord + direction, piece_number + 1)
+
+        path.pop()
+
+    for direction in Direction:
+        dfs(coord + direction, 1)
+    
+    return possible_moves
+
+def get_possible_moves(state: dict, player: PlayerColor):
+    """
+    Get all possible moves for a particular player.
+    Return a list of PlaceAction
+    """
+    moveset = set()
+
+    for coord, colour in state.items():
+        if colour == player:
+            moveset = moveset.union(get_possible_moves_from_coord(state, coord))
+
+    return [set_to_place_action(piece) for piece in moveset]
+
+def get_next_state(current_state: dict, piece: PlaceAction, color: PlayerColor):
+    """
+    Gets in a state and a piece (PlaceAction), and returns the next state.
+    Accounts for lines being cleared.
+    Assumes the pieces(PlaceAction) being placed is valid.
+    """
+    
+    # cleared rows / cols
+    cleared_rows = set()
+    cleared_cols = set()
+
+    current_state_copy = current_state.copy()
+    # print("COPY", current_state_copy)
+
+    # places new red squares
+    for square in piece.coords:
+        # print(square)
+        current_state_copy[square] = color
+
+    # getting a grid of filled squares
+    grid = [[True if Coord(row, col) in current_state_copy else False for col in range(BOARD_N)] for row in range(BOARD_N)]
+    # pprint(grid)
+    
+    # gets all full rows
+    for row in range(BOARD_N):
+        is_full_row = True
+        for col in range(BOARD_N):
+            if not grid[row][col]:
+                is_full_row = False
+                break
+        
+        if is_full_row:
+            cleared_rows.add(row)
+
+    # gets all full columns
+    for col in range(BOARD_N):
+        is_full_col = True
+        for row in range(BOARD_N):
+            if not grid[row][col]:
+                is_full_col = False
+                break
+        
+        if is_full_col:
+            cleared_cols.add(col)
+        
+    # add in all squares except for ones that are cleared
+    next_state = {coord: current_state_copy[coord] for coord in current_state_copy if coord.r not in cleared_rows and coord.c not in cleared_cols}
+    return next_state
+
+def set_to_place_action(coord_set):
+    """
+    Turns a set/frozenset into a PlaceAction
+    """
+    return PlaceAction(*coord_set)
---
+++ monte_carlo_agent/program.py
@@ -0,0 +1,169 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part B: Game Playing Agent
+
+import math
+from helpers import get_next_state, get_possible_moves
+from referee.game import PlayerColor, Action, PlaceAction, Coord, BOARD_N
+from referee.game.coord import Direction
+import random
+
+TOTAL_RUNS = 100
+MAX_TURNS = 150
+C = 2
+
+class Agent:
+    """
+    This class is the "entry point" for your agent, providing an interface to
+    respond to various Tetress game events.
+    """
+
+    def __init__(self, color: PlayerColor, **referee: dict):
+        """
+        This constructor method runs when the referee instantiates the agent.
+        Any setup and/or precomputation should be done here.
+        """
+        self._color = color
+        self.first_turn = True
+        self.monte_carlo_root = None
+
+    def action(self, **referee: dict) -> Action:
+        """
+        Do a monte carlo tree search given a node
+        """
+        # RED = 0
+        # BLUE = 1
+
+        # initial move
+        if self.first_turn:
+            self.first_turn = False
+            match self._color:
+                case PlayerColor.RED:
+                    # print("Testing: RED is playing a PLACE action")
+                    return PlaceAction(
+                        Coord(3, 3), 
+                        Coord(3, 4), 
+                        Coord(4, 3), 
+                        Coord(4, 4)
+                    )
+                case PlayerColor.BLUE:
+                    # print("Testing: BLUE is playing a PLACE action")
+                    return PlaceAction(
+                        Coord(2, 3), 
+                        Coord(2, 4), 
+                        Coord(2, 5), 
+                        Coord(2, 6)
+                    )
+
+        # all moves after the first one 
+        # (MONTE CARLO)
+
+        runs = 0
+        while (runs < TOTAL_RUNS):
+            runs += 1
+
+            node = self.monte_carlo_root
+
+            # selection
+            while node.children:
+
+                # select the highest UBC1 node
+                highest_ubc1_node = None
+                highest_ubc1 = -1
+                for child in node.children:
+                    if child.ubc1 > highest_ubc1:
+                        highest_ubc1 = child.ubc1
+                        highest_ubc1_node = child
+                    # TODO can optimise by short circuiting
+
+                # make the highest UBC1 node the next node in the chain
+                node = highest_ubc1_node
+            
+            # now should be at a leaf node
+            if node.n == 0:
+                # rollout
+                pass
+            else:
+                # expand
+                # rollout
+                pass
+            
+            # backpropogate
+
+
+        possible_moves = get_possible_moves(self.current_state, self._color)
+        size = len(possible_moves)
+        assert size > 0
+        return possible_moves[random.randrange(0, size)]
+
+    def update(self, color: PlayerColor, action: Action, **referee: dict):
+        """
+        Updating the state
+        """
+        self.monte_carlo_root = MonteCarloNode(get_next_state(self.current_state, action, color))
+
+
+class MonteCarloNode:
+    def __init__(self, state: dict, parent):
+        self.state = state
+        self.t = 0
+        self.n = 0
+        self.ubc1 = float('inf')
+        self.children = []
+        self.parent = parent
+
+    def increase_t(self, score):
+        self.t += score
+        self.n += 1
+    
+    def calculate_ubc1(self, runs):
+        assert self.n != 0
+        self.ubc1 = (self.t / self.n) + C * math.sqrt(math.log(runs / self.n))
+    
+    def append_child(self, node):
+        self.children.append(node)
+
+def rollout(state: dict, current_turn: int, player: PlayerColor):
+    # keep rolling out until one player has no available plays left or the max turns has been reached
+    current_player = player
+    current_state = state
+    while current_turn <= MAX_TURNS:
+        possible_moves = get_possible_moves(current_state, current_player)
+        if not possible_moves:
+            # this player has no possible moves, so the other player wins
+            if player == PlayerColor.BLUE:
+                # RED WINS
+                return 0
+            else:
+                # BLUE WINS
+                return 1
+            
+        # there are moves, so pick one at random and continue the rollout
+        next_action = possible_moves[random.randrange(0, len(possible_moves))]
+        current_state = get_next_state(current_state, next_action)
+
+        current_turn += 1
+        if current_player == PlayerColor.RED:
+            current_player = PlayerColor.BLUE
+        else:
+            current_player = PlayerColor.RED
+
+    # max turns has been reached
+    # return based on whoever has more squares
+    reds = 0
+    blues = 0
+    for _, color in current_state:
+        if color == PlayerColor.RED:
+            reds += 1
+        else:
+            blues += 1
+    
+    if reds > blues:
+        # red won
+        return 0
+    elif reds == blues:
+        # tie
+        return 0.5
+    else:
+        # blue won
+        return 1
+            
